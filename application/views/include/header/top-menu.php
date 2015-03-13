081befb4a941aeb7e127c234871463e29e1bab2f
TojoAntonio <a.tojo.antonio@gmail.com>
2015-03-13 14:39:31 +0300

    ajout bouton login

--- application/views/include/header/top-menu.php
+++ application/views/include/header/top-menu.php
@@ -6,8 +6,9 @@
                         <ul class="nav menu nav-pills">
                             <?php if($this->session->userdata('user') == null){ ?>
                             <!--<li class="item-143"><a class="modalToggle" id="login" href="#modal">Login</a></li>-->
-                            <li class="item-143"><a class="btn btn-info" href="#modal">Login</a> </li>
-                            <!--<li class="item-178"><a href="<?php echo base_url(); ?>UserController">Créer un compte</a>-->
+                            <a class="btn btn-info" href="#modal">Login</a>
+                            <!-- <li class="item"> </li>
+                            <li class="item-178"><a href="<?php echo base_url(); ?>UserController">Créer un compte</a>-->
                             <?php } else{ ?>
                             <li class="item-143"><strong><i><?php print_r($this->session->userdata('user')[0]->U_PSEUDO) ?></i></strong>
                             <li class="item-178"><a class="modalToggle" href="<?php echo base_url(); ?>index.php/UserController/disconnect">Se déconnecter</a>
